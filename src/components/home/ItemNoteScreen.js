b7cfc2e313ea1f1e49777354dcaf0d92f632a6d6
chuoi <tungnd-0844>
2019-07-09 16:23:44 +0700

    tg

--- src/components/home/ItemNoteScreen.js
+++ src/components/home/ItemNoteScreen.js
@@ -7,44 +7,144 @@
  */
 
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View, Image } from "react-native";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Alert,
+  ToastAndroid,
+  TouchableOpacity
+} from "react-native";
 import MaterialIcon from "react-native-vector-icons/MaterialIcons";
 import ActionButton from "react-native-action-button";
 import Icon from "react-native-vector-icons/Ionicons";
+import Swipeout from "react-native-swipeout";
 
-export default class HomeScreen extends Component {
+export default class ItemNoteScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeRowKey: null
+    };
+  }
+
+  onDeleteNote = item => {
+    this.props.onPress(item);
+  };
+
   render() {
     const { item } = this.props;
+    const swipeSetting = {
+      autoClose: true,
+      onClose: (secId, rowId, direction) => {
+        this.setState({ activeRowKey: null });
+      },
+      onOpen: (secId, rowId, direction) => {
+        this.setState({ activeRowKey: item.id });
+      },
+      right: [
+        {
+          text: "Done",
+          backgroundColor: "green",
+          underlayColor: "rgba(0, 0, 0, 1, 0.6)",
+          onPress: () => {
+            // const deletingRow = item.complete;
+            this.props.parentFlatList.onToggleNote(item);
+            //  ToastAndroid.show(deletingRow, ToastAndroid.SHORT);
+            // Alert.alert(
+            //   "Alert",
+            //   "Bạn có chắc chắn muốn xóa???",
+            //   [
+            //     { text: "No", style: "cancel" },
+            //     {
+            //       text: "Yes",
+            //       onPress: () => {
+            //
+            //         // this.onDeleteNote(item);
+            //         // this.props.parentFlatList.refreshNoteList(deletingRow);
+            //       }
+            //     }
+            //   ],
+            //   { cancelable: true }
+            // );
+          }
+        },
+        {
+          text: "Delete",
+          backgroundColor: "red",
+          underlayColor: "rgba(0, 0, 0, 1, 0.6)",
+          onPress: () => {
+            const deletingRow = this.state.activeRowKey;
+            Alert.alert(
+              "Alert",
+              "Bạn có chắc chắn muốn xóa???",
+              [
+                { text: "No", style: "cancel" },
+                {
+                  text: "Yes",
+                  onPress: () => {
+                    this.onDeleteNote(item);
+                    this.props.parentFlatList.refreshNoteList(deletingRow);
+                  }
+                }
+              ],
+              { cancelable: true }
+            );
+          }
+        }
+      ],
+      rowId: item.id,
+      sectionId: 1
+    };
     return (
-      <View style={{ marginLeft: 8, marginRight: 8, marginTop: 8 }}>
-        <View
-          style={{ height: 10, backgroundColor: item.background, elevation: 2 }}
-        />
-        <View
-          style={{
-            flexDirection: "row",
-            backgroundColor: item.background,
-            elevation: 1
-          }}
-        >
-          <View style={{ flex: 1, margin: 8 }}>
-            <View style={{ flexDirection: "row" }}>
-              <Text>{item.date}</Text>
-              <MaterialIcon name="room" size={20} />
-              <MaterialIcon name="settings-voice" size={20} />
-            </View>
-
-            <Text style={{ fontSize: 20, color: "black" }}>
-              {item.description}
-            </Text>
-          </View>
-          <Image
-            source={{
-              uri: item.image
+      <View
+        style={[
+          { marginLeft: 8, marginRight: 8, marginTop: 8 },
+          item.complete ? styles.completed : {}
+        ]}
+      >
+        <Swipeout {...swipeSetting}>
+          <View
+            style={{
+              height: 10,
+              backgroundColor: item.background,
+              elevation: 2
             }}
-            style={{ width: 75, height: 75, margin: 8 }}
           />
-        </View>
+          <TouchableOpacity
+            onPress={() =>
+              this.props.parentFlatList._handlerNavigationDetailNote(item)
+            }
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                backgroundColor: item.background,
+                elevation: 1
+              }}
+            >
+              <View style={{ flex: 1, margin: 8 }}>
+                <View style={{ flexDirection: "row" }}>
+                  <Text>{item.date}</Text>
+                  <MaterialIcon name="room" size={20} />
+                  <MaterialIcon name="settings-voice" size={20} />
+                </View>
+
+                <Text style={[{ fontSize: 20, color: "black" }]}>
+                  {item.description}
+                </Text>
+              </View>
+              <Image
+                source={{
+                  uri: item.image
+                }}
+                style={{ width: 75, height: 75, margin: 8 }}
+              />
+            </View>
+          </TouchableOpacity>
+        </Swipeout>
       </View>
     );
   }
@@ -66,5 +166,17 @@ const styles = StyleSheet.create({
     textAlign: "center",
     color: "#333333",
     marginBottom: 5
+  },
+  completed: {
+    opacity: 0.5
+  },
+  verticalLine: {
+    borderBottomColor: "black",
+    borderBottomWidth: 2,
+    marginLeft: 10,
+    //  width: "100%",
+    position: "absolute",
+    // marginTop: 15,
+    fontWeight: "bold"
   }
 });
---
+++ src/components/search/SearchScreen.js
@@ -0,0 +1,146 @@
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity
+} from "react-native";
+import { SearchBar } from "react-native-elements";
+import Icon from "react-native-vector-icons/Ionicons";
+import ItemNoteScreen from "../home/ItemNoteScreen";
+import { openDatabase } from "react-native-sqlite-storage";
+
+var db = openDatabase({ name: "NoteDatabase.db" });
+export default class SearchScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      note: "",
+      listNotes: []
+    };
+  }
+
+  static navigationOptions = ({ navigation }) => {
+    return {
+      header: null
+    };
+  };
+
+  _searchFilterFunction = async text => {
+    await this.setState({ note: text });
+    const { note } = this.state;
+    db.transaction(tx => {
+      tx.executeSql(
+        "SELECT * FROM table_note where description = ?",
+        [note],
+        (tx, results) => {
+          var temp = [];
+          for (let i = 0; i < results.rows.length; ++i) {
+            temp.push(results.rows.item(i));
+          }
+          this.setState({
+            listNotes: temp
+          });
+        }
+      );
+    });
+  };
+
+  onDeleteNote = item => {
+    db.transaction(tx => {
+      tx.executeSql(
+        "DELETE FROM table_note where id=?",
+        [item.id],
+        (tx, results) => {
+          if (results.rowsAffected > 0) {
+            this.getAllNotes();
+            ToastAndroid.show("Bạn đã xóa thành công", ToastAndroid.SHORT);
+          } else {
+            ToastAndroid.show("Xoá thất bại", ToastAndroid.SHORT);
+          }
+        }
+      );
+    });
+  };
+
+  onToggleNote = item => {
+    const { id, complete } = item;
+    var a = complete;
+    if (a === 1) {
+      a = 0;
+    } else {
+      a = 1;
+    }
+    db.transaction(tx => {
+      tx.executeSql(
+        "UPDATE table_note set complete=? where id=?",
+        [a, id],
+        (tx, results) => {
+          if (results.rowsAffected > 0) {
+            this.getAllNotes();
+            ToastAndroid.show("Bạn đã thay đổi thành công", ToastAndroid.SHORT);
+          } else {
+            ToastAndroid.show("Thay đổi thất bại", ToastAndroid.SHORT);
+          }
+        }
+      );
+    });
+  };
+
+  _handlerNavigationDetailNote = item => {
+    this.props.navigation.navigate("DetailNote", {
+      ITEM: item
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <SearchBar
+          placeholder="Type Here..."
+          lightTheme
+          onChangeText={this._searchFilterFunction}
+          round
+          value={this.state.note}
+          autoCorrect={false}
+        />
+        <FlatList
+          data={this.state.listNotes}
+          renderItem={({ item, index }) => {
+            return (
+              <ItemNoteScreen
+                item={item}
+                index={index}
+                onPress={this.onDeleteNote}
+                parentFlatList={this}
+              />
+            );
+          }}
+          keyExtractor={({ id }, index) => index.toString()}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  imagPath: {
+    width: 120,
+    height: 150,
+    position: "absolute"
+  },
+  textName: {
+    color: "white",
+    fontSize: 14,
+    marginLeft: 6
+  },
+  textJob: {
+    color: "white",
+    fontSize: 12,
+    margin: 6
+  }
+});
